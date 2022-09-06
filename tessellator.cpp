b4e865fe301adba9d75a1cf936bf4081b92b2737
2kai2kai2 <2kai2kai2@gmail.com>
2022-09-06 17:54:18 -0400

    hmm

--- tessellator.cpp
+++ tessellator.cpp
@@ -6,8 +6,7 @@
 
 const long long HEIGHT = 1024;
 const long long WIDTH = 1024;
-const long long EDGE_MIN = 200;
-const long long EDGE_VAR = 100;
+const long long CELL_SIZE = 128;
 
 struct Point {
     long long x;
@@ -20,15 +19,6 @@ struct Point {
         b->links.insert(a);
     }
 };
-
-struct ExposedEdge {
-    Point* a;
-    Point* b;
-
-    /* Always placing triangle in the positive rotation with p1 as origin */
-    ExposedEdge(Point* p1, Point* p2) : a(p1), b(p2) {}
-};
-
 struct Triangle {
     Point* a;
     Point* b;
@@ -45,66 +35,62 @@ struct Triangle {
     }
 };
 
-int main() {
-    srand(time(NULL));
-    std::vector<Point*> points;
-    std::vector<Triangle> triangles;
-    std::list<ExposedEdge> edges;
+struct Space {
+    double width;
+    double height;
+    size_t cell_width;
+    size_t cell_height;
+    Point*** arr;
 
-    // Place starting points
-    points.push_back(new Point(WIDTH / 2, HEIGHT / 2));
-    size_t _start_len = EDGE_MIN + rand() % EDGE_VAR;
-    double _start_angle = remainder(rand() / 1000000.0, M_2_PI);
-    points.push_back(new Point(WIDTH / 2 + _start_len * cos(_start_angle),
-                               HEIGHT / 2 + _start_len * sin(_start_angle)));
-    establish_links(points[0], points[1]);
-    edges.emplace_back(points[0], points[1]);
-    edges.emplace_back(points[1], points[0]);
-
-    for (int j = 0; j < 10 && !edges.empty(); ++j) {
-        double dx = edges.front().b->x - edges.front().a->x;
-        double dy = edges.front().b->y - edges.front().a->y;
-        double edge_angle = atan2(dy, dx);
-        Point* new_point = nullptr;
-
-        for (auto itr = edges.begin()++; itr != edges.end(); ++itr) {
-            if (itr->b == edges.front().a && itr->a != edges.front().b) {
-                // This means that there is an edge facing the opposite
-                // direction attached to a. check if it's close!
-                double p_dx = itr->a->x - edges.front().a->x;
-                double p_dy = itr->a->y - edges.front().a->y;
-                double p_angle = atan2(p_dy, p_dx);
-                if (fabs(edge_angle - p_angle) < M_PI / 3) {
-                    new_point = itr->a;
-                    edges.erase(itr);
-                    break;
-                }
+    Space(double width, double height)
+        : width(width), height(height), cell_width(width / CELL_SIZE),
+          cell_height(height / CELL_SIZE) {
+        arr = new Point**[cell_width];
+        for (size_t col = 0; col < cell_width; ++col)
+            arr[col] = new Point* [cell_height] { nullptr };
+    }
+    ~Space() {
+        for (size_t col = 0; col < cell_width; ++col) {
+            for (size_t row = 0; row < cell_height; ++row) {
+                delete arr[col][row];
             }
+            delete[] arr[col];
         }
-        if (!new_point) {
-            double angle = fabs(remainder(rand() / 1000000.0, M_PI / 3));
-            std::cout << edge_angle << ' ' << angle << std::endl;
-
-            size_t new_length = rand() % EDGE_VAR + EDGE_MIN;
-            long long new_x = edges.front().a->x +
-                              new_length * cos(edge_angle + M_PI_2 + angle);
-            long long new_y = edges.front().a->y +
-                              new_length * sin(edge_angle + M_PI_2 + angle);
-            new_point = new Point(new_x, new_y);
-
-            points.push_back(new_point);
-            if (new_x >= 0 && new_y >= 0 && new_x < WIDTH && new_y < HEIGHT) {
-                edges.emplace_back(edges.front().a, new_point);
-                edges.emplace_back(new_point, edges.front().b);
-            }
-        }
-        triangles.emplace_back(edges.front().a, edges.front().b, new_point);
-        establish_links(edges.front().a, new_point);
-        establish_links(edges.front().b, new_point);
-
-        edges.pop_front();
+        delete[] arr;
     }
 
+    Point* get(size_t cell_x, size_t cell_y) const {
+        if (cell_x >= cell_width || cell_y >= cell_height)
+            throw std::out_of_range("Invalid cell number (" +
+                                    std::to_string(cell_x) + ", " +
+                                    std::to_string(cell_y) + ") accessed.");
+        return arr[cell_x][cell_y];
+    }
+
+    void populate() {
+        for (size_t col = 0; col < cell_width; ++col) {
+            for (size_t row = 0; row < cell_height; ++row) {
+                arr[col][row] = new Point(col * CELL_SIZE + rand() % CELL_SIZE,
+                                          row * CELL_SIZE + rand() % CELL_SIZE);
+            }
+        }
+    }
+};
+
+int main() {
+    srand(time(NULL));
+    std::vector<Triangle> triangles;
+
+    Space space(WIDTH, HEIGHT);
+    space.populate();
+
+    for (size_t col = 0; col < space.cell_width - 1; ++col) {
+        for (size_t row = 0; row < space.cell_height - 1; ++row) {
+            triangles.emplace_back(space.get(col, row), space.get(col + 1, row), space.get(col, row + 1));
+        }
+    }
+
+    // Draw triangles
     SVG svg(HEIGHT, WIDTH);
     for (const Triangle& tri : triangles)
         svg.polygons.push_back(tri.to_poly());
