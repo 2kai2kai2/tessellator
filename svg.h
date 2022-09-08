94426dbc4707fa69d92d5963bc9aacdea30daa3e
2kai2kai2 <2kai2kai2@gmail.com>
2022-09-07 20:29:44 -0400

    Definitely does stuff now

--- svg.h
+++ svg.h
@@ -1,34 +1,82 @@
 #include <fstream>
 #include <iostream>
-#include <set>
 #include <list>
+#include <set>
 
-struct SVG_Polygon {
+struct SVG_Shape {
+    virtual ~SVG_Shape() {}
+
+    virtual std::ostream& print(std::ostream& ostr) const = 0;
+    friend std::ostream& operator<<(std::ostream& a, const SVG_Shape& b) {
+        return b.print(a);
+    }
+};
+
+struct SVG_Polygon : SVG_Shape {
     std::list<std::pair<long long, long long>> points;
     std::string color;
 
-    friend std::ostream& operator<<(std::ostream& a, const SVG_Polygon& b) {
+    std::ostream& print(std::ostream& a) const override {
         a << "<polygon points=\"";
-        for (const std::pair<long long, long long>& p : b.points)
+        for (const std::pair<long long, long long>& p : points)
             a << p.first << ',' << p.second << ' ';
         a << "\" style=\"";
-        if (!b.color.empty())
-            a << "fill:" << b.color << ";";
+        if (!color.empty())
+            a << "fill:" << color << ";";
         return a << "\" />";
+    }
+};
+
+struct SVG_Circle : SVG_Shape {
+    double cx;
+    double cy;
+    double radius;
+    std::string stroke;
+    int stroke_width;
+    double stroke_opacity;
+    std::string fill;
+    double fill_opacity;
+
+    SVG_Circle()
+        : cx(0), cy(0), radius(0), stroke_width(-1), stroke_opacity(1),
+          fill_opacity(1) {}
+    SVG_Circle(double cx, double cy, double radius)
+        : cx(cx), cy(cy), radius(radius), stroke_width(-1), stroke_opacity(1),
+          fill_opacity(1) {}
+
+    std::ostream& print(std::ostream& a) const override {
+        a << "<circle cx=\"" << cx << "\" cy=\"" << cy << "\" r=\"" << radius
+          << "\" ";
+        if (!stroke.empty())
+            a << "stroke=\"" << stroke << "\" ";
+        if (stroke_width >= 0)
+            a << "stroke-width=\"" << stroke_width << "\" ";
+        if (stroke_opacity < 1)
+            a << "stroke-opacity=\"" << stroke_opacity << "\" ";
+        if (!fill.empty())
+            a << "fill=\"" << fill << "\" ";
+        if (fill_opacity < 1)
+            a << "fill-opacity=\"" << fill_opacity << "\" ";
+        return a << "/>";
     }
 };
 
 struct SVG {
     size_t height;
     size_t width;
-    std::list<SVG_Polygon> polygons;
+    std::list<SVG_Shape*> shapes;
 
     SVG(size_t height, size_t width) : height(height), width(width) {}
+    ~SVG() {
+        for (SVG_Shape* shape : shapes)
+            delete shape;
+    }
 
     friend std::ostream& operator<<(std::ostream& a, const SVG& b) {
-        a << "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"" << b.height << "\" width=\"" << b.width << "\">\n";
-        for (const SVG_Polygon& poly : b.polygons)
-            a << poly << '\n';
+        a << "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"" << b.height
+          << "\" width=\"" << b.width << "\">\n";
+        for (const SVG_Shape* shape : b.shapes)
+            a << *shape << '\n';
         return a << "</svg>";
     }
 };
--- tessellator.cpp
+++ tessellator.cpp
@@ -1,22 +1,94 @@
 #include "svg.h"
+#include <algorithm>
 #include <cmath>
 #include <list>
+#include <map>
 #include <random>
 #include <vector>
 
 const long long HEIGHT = 1024;
 const long long WIDTH = 1024;
-const long long CELL_SIZE = 128;
+const long long MIN_RADIUS = 16;
+const long long MAX_RADIUS = 128;
+
+typedef std::pair<double, double> Coord;
+
+double frandrange(double min, double max) {
+    return min + abs(remainder(fabs(rand() / 1000000.0), max - min));
+}
 
 struct Point {
-    long long x;
-    long long y;
-    std::set<Point*> links;
-    Point(long long x, long long y) : x(x), y(y) {}
+    double x;
+    double y;
+    double radius;
+    std::map<Point*, unsigned char> links;
+    Point(double x, double y, double radius) : x(x), y(y), radius(radius) {}
+    Point(const Coord& loc, double radius)
+        : x(loc.first), y(loc.second), radius(radius) {}
 
     friend void establish_links(Point* a, Point* b) {
-        a->links.insert(b);
-        b->links.insert(a);
+        a->links.emplace(b, 0);
+        b->links.emplace(a, 0);
+    }
+    friend void increment_links(Point* a, Point* b, Point* c) {
+        ++(a->links[b]);
+        ++(a->links[c]);
+        ++(b->links[a]);
+        ++(b->links[c]);
+        ++(c->links[a]);
+        ++(c->links[b]);
+    }
+
+    SVG_Circle to_circle() const {
+        SVG_Circle out{x, y, radius};
+        out.stroke = "black";
+        out.stroke_width = 2;
+        out.fill_opacity = 0;
+        return out;
+    }
+
+    inline double dist2(const Point* other) const {
+        double dx = x - other->x;
+        double dy = y - other->y;
+        return dx * dx + dy * dy;
+    }
+    inline double dist2(const Coord& other) const {
+        double dx = x - other.first;
+        double dy = y - other.second;
+        return dx * dx + dy * dy;
+    }
+};
+
+std::pair<Coord, Coord> intersects(const Point* p1, const Point* p2,
+                                   double add_radius = 0.0) {
+    // https://math.stackexchange.com/a/1367732
+    double dx = p2->x - p1->x;
+    double dy = p2->y - p1->y;
+    double R2 = dx * dx + dy * dy;
+    if (R2 == 0)
+        return {};
+    double r1 = p1->radius + add_radius;
+    double r2 = p2->radius + add_radius;
+    // a = (r_1^2 - r_2^2) / R2
+    double a = (r1 * r1 - r2 * r2) / R2;
+    double base_x = (p1->x + p2->x) / 2 + a / 2 * dx;
+    double base_y = (p1->y + p2->y) / 2 + a / 2 * dy;
+    // b = that complicated square root in the link including 1/2
+    double b = sqrt(2 * (r1 * r1 + r2 * r2) / R2 - a * a - 1) / 2;
+    return std::pair<std::pair<double, double>, std::pair<double, double>>(
+        {base_x + b * dy, base_y + b * -dx},
+        {base_x + b * -dy, base_y + b * dx});
+}
+struct ExposedEdge {
+    Point* a;
+    Point* b;
+    unsigned char attempts;
+
+    /* Always placing triangle in the positive rotation with p1 as origin */
+    ExposedEdge(Point* p1, Point* p2) : a(p1), b(p2), attempts(10) {}
+
+    bool operator==(const ExposedEdge& other) const {
+        return a == other.a && b == other.b;
     }
 };
 struct Triangle {
@@ -40,64 +112,145 @@ struct Space {
     double height;
     size_t cell_width;
     size_t cell_height;
-    Point*** arr;
+    std::vector<Point*> all;
+    std::vector<std::vector<std::list<Point*>>> arr;
 
     Space(double width, double height)
-        : width(width), height(height), cell_width(width / CELL_SIZE),
-          cell_height(height / CELL_SIZE) {
-        arr = new Point**[cell_width];
-        for (size_t col = 0; col < cell_width; ++col)
-            arr[col] = new Point* [cell_height] { nullptr };
-    }
-    ~Space() {
-        for (size_t col = 0; col < cell_width; ++col) {
-            for (size_t row = 0; row < cell_height; ++row) {
-                delete arr[col][row];
-            }
-            delete[] arr[col];
-        }
-        delete[] arr;
+        : width(width), height(height), cell_width(width / MAX_RADIUS),
+          cell_height(height / MAX_RADIUS),
+          arr(cell_width, std::vector<std::list<Point*>>(cell_height,
+                                                         std::list<Point*>())) {
     }
 
-    Point* get(size_t cell_x, size_t cell_y) const {
-        if (cell_x >= cell_width || cell_y >= cell_height)
-            throw std::out_of_range("Invalid cell number (" +
-                                    std::to_string(cell_x) + ", " +
-                                    std::to_string(cell_y) + ") accessed.");
-        return arr[cell_x][cell_y];
+    void add(double x, double y, double radius) {
+        all.push_back(new Point(x, y, radius));
+        long long cell_x = x / MAX_RADIUS;
+        long long cell_y = y / MAX_RADIUS;
+        if (0 <= cell_x && cell_x < cell_width && 0 <= cell_y &&
+            cell_y < cell_height)
+            arr[x / MAX_RADIUS][y / MAX_RADIUS].push_back(all.back());
     }
 
-    void populate() {
-        for (size_t col = 0; col < cell_width; ++col) {
-            for (size_t row = 0; row < cell_height; ++row) {
-                arr[col][row] = new Point(col * CELL_SIZE + rand() % CELL_SIZE,
-                                          row * CELL_SIZE + rand() % CELL_SIZE);
+    std::vector<Triangle> populate() {
+        std::vector<Triangle> out;
+        std::list<ExposedEdge> edges;
+        double first_radius = frandrange(MIN_RADIUS, MAX_RADIUS);
+        add(width / 2, height / 2, first_radius);
+        double second_radius = frandrange(MIN_RADIUS, MAX_RADIUS);
+        double second_angle = frandrange(0, M_2_PI);
+        add(width / 2 + (first_radius + second_radius) * cos(second_angle),
+            height / 2 + (first_radius + second_radius) * sin(second_angle),
+            second_radius);
+        establish_links(all[0], all[1]);
+        edges.emplace_back(all[0], all[1]);
+        edges.emplace_back(all[1], all[0]);
+        while (!edges.empty()) {
+            double new_radius = frandrange(MIN_RADIUS, MAX_RADIUS);
+            Coord potential =
+                intersects(edges.front().a, edges.front().b, new_radius).first;
+            // Now check if we can connect 3 in a triangle
+            for (Point* p : all) {
+                if (p->dist2(potential) < pow(MIN_RADIUS, 2)) {
+                    // They are close
+                    for (auto link : edges.front().a->links) {
+                        if (link.second < 2 && link.first == p) {
+                            // Epic! we can use this
+                            establish_links(p, edges.front().b);
+                            increment_links(p, edges.front().a,
+                                            edges.front().b);
+                            out.emplace_back(p, edges.front().a,
+                                             edges.front().b);
+
+                            if (p->x >= 0 && p->y >= 0 && p->x < width &&
+                                p->y < height) {
+                                // Remove the matching edge
+                                edges.remove(ExposedEdge(p, edges.front().a));
+                                // Add a new edge if applicable
+                                edges.emplace_back(p, edges.front().b);
+                            }
+                            goto endloop;
+                        }
+                    }
+                    for (auto link : edges.front().b->links) {
+                        if (link.second < 2 && link.first == p) {
+                            // Epic! we can use this
+                            establish_links(p, edges.front().a);
+                            increment_links(p, edges.front().a,
+                                            edges.front().b);
+                            out.emplace_back(p, edges.front().a,
+                                             edges.front().b);
+
+                            if (p->x >= 0 && p->y >= 0 && p->x < width &&
+                                p->y < height) {
+                                // Remove the matching edge
+                                edges.remove(ExposedEdge(edges.front().b, p));
+                                // Add a new edge if applicable
+                                edges.emplace_back(edges.front().b, p);
+                            }
+                            goto endloop;
+                        }
+                    }
+                }
             }
+            // Check if this overlaps with anything
+            for (const Point* p : all) {
+                if (p->dist2(potential) < pow(p->radius + new_radius - 2, 2)) {
+                    // Bad overlap
+                    if (edges.front().attempts > 0) {
+                        // Put it for later
+                        edges.emplace_back(edges.front());
+                        --edges.back().attempts;
+                    }
+                    goto endloop;
+                }
+            }
+            // If not, keep going
+            add(potential.first, potential.second, new_radius);
+            establish_links(all.back(), edges.front().a);
+            establish_links(all.back(), edges.front().b);
+            increment_links(all.back(), edges.front().a, edges.front().b);
+            out.emplace_back(all.back(), edges.front().a, edges.front().b);
+            if (all.back()->x >= 0 && all.back()->y >= 0 &&
+                all.back()->x < width && all.back()->y < height) {
+                edges.emplace_back(edges.front().a, all.back());
+                edges.emplace_back(all.back(), edges.front().b);
+                // Check if we can add any new edges
+                for (Point* p : all) {
+                    if (p->x < 0 || width <= p->x || p->y < 0 || height <= p->y)
+                        continue;
+                    else if (p == all.back() || all.back()->links.count(p))
+                        continue;
+                    else if (p->dist2(potential) <
+                             pow(p->radius + new_radius + MIN_RADIUS, 2)) {
+                        // These could have an edge
+                        establish_links(p, all.back());
+                        edges.emplace_back(p, all.back());
+                        edges.emplace_back(all.back(), p);
+                    }
+                }
+            }
+
+        endloop:
+            edges.pop_front();
         }
+        return out;
     }
 };
 
 int main() {
     srand(time(NULL));
-    std::vector<Triangle> triangles;
 
     Space space(WIDTH, HEIGHT);
-    space.populate();
-
-    for (size_t col = 0; col < space.cell_width; ++col) {
-        for (size_t row = 0; row < space.cell_height; ++row) {
-            if (col + 1 < space.cell_width && row + 1 < space.cell_height)
-                triangles.emplace_back(space.get(col, row), space.get(col + 1, row), space.get(col, row + 1));
-            if (col >= 1 && row >= 1)
-                triangles.emplace_back(space.get(col, row), space.get(col - 1, row), space.get(col, row - 1));
-            
-        }
-    }
+    std::vector<Triangle> triangles = space.populate();
 
     // Draw triangles
     SVG svg(HEIGHT, WIDTH);
     for (const Triangle& tri : triangles)
-        svg.polygons.push_back(tri.to_poly());
+        svg.shapes.push_back(new SVG_Polygon(tri.to_poly()));
+
+    // Overlay circles
+    for (const Point* point : space.all)
+        svg.shapes.push_back(new SVG_Circle(point->to_circle()));
 
     std::ofstream file("out.svg");
     file << "<!DOCTYPE svg>\n" << svg << std::endl;
